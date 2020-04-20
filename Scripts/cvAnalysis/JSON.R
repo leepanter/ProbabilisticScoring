93a4d98b2d9d238627470d39af3d3e6d0de45fd3
Lee Panter <lee.panter@yahoo.com>
2020-04-19 21:50:23 -0600

    Manifest Creator  JSON.R

---
+++ Scripts/cvAnalysis/JSON.R
@@ -0,0 +1,14 @@
+# JSON.r
+# Create .json files for deployment
+
+
+rsconnect::writeManifest(appDir = "/Users/lee/Documents/GitHub/ProbabilisticScoring/Scripts/cvAnalysis",
+                         appFiles = c("functionsProbScoreCalc.R",
+                                      "functionsScoringAnalysis.R",
+                                      "functionsWeightCalculations.R",
+                                      "MasterCVanalysis.R",
+                                      "MasterCVanalysis2.R",
+                                      "test.Rmd",
+                                      "phq9Subset.Rdata",
+                                      "functionsDataImportProcessing")
+                         )
