77e0e166329b73436bacbec3905e0efb6cf89c17
Lee Panter <lee.panter@yahoo.com>
2020-04-26 23:18:46 -0600

    Post Remote Compile CV2

--- Scripts/cvAnalysis/MasterCVAnalysis2.R
+++ Scripts/cvAnalysis/MasterCVAnalysis2.R
@@ -55,7 +55,7 @@ source(file="/Users/lee/Documents/GitHub/ProbabilisticScoring/Scripts/cvAnalysis
 #-------------------------------------------------------------------------#
 ## This portion of code will subsample the given data.  Each subsample will include all colums of data, and the number of rows in each sub-sample will range from one to 2495.  Each subsample will be stored in its own list item so that a subsample distribution of weights may be generated in the following step.
 
-number.samples=25
+number.samples=100
 sample.length=number.samples+2
 sample.vec.k.sets=df.set.info$df.k.sets
 sample.vec.k.sets=sample.vec.k.sets[-c(1,1245)]
@@ -292,9 +292,17 @@ for(i in 1:sample.length){
 
 plot(accuracy.Sup3~length.boot.weights)
 
+df=data.frame(accuracy.Sup3, length.boot.weights)
 
+write.csv(df, file = "/Users/lee/Desktop/df_acc_100_sup2.csv")
 
+lmod=lm(accuracy.Sup3~length.boot.weights, data = df)
+lmods=summary(lmod)
 
+plot(accuracy.Sup3~length.boot.weights, 
+     xlab="Training Data Length", 
+     ylab = "Accuracy vs Probabilistically Outcom SUP2")
+abline(a=lmods$coefficients[1,1], b=lmods$coefficients[2,1])
 
 
 
